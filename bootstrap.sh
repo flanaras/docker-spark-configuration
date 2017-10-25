c585eff91a1a8a1c984613804477c309c5790786
Philip Lanaras <flanaras@outlook.com>
2017-10-25 15:22:10 +0200

    invoke master

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,11 @@
 /usr/sbin/sshd
 
 # In
+## setting spark defaults
+cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
+
+$SPARK_HOME/sbin/start-master.sh
+$SPARK_HOME/sbin/start-history-server.sh
 
 # Loop
 CMD=${1:-"exit 0"}
